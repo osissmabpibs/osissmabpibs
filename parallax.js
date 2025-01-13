a03ffaaa15a0912c205b4f64857ccf8750f45e30
osissmabpibs <148605583+osissmabpibs@users.noreply.github.com>
2025-01-13 20:56:55 +0700

    Update parallax.js
    Signed-off-by: osissmabpibs <148605583+osissmabpibs@users.noreply.github.com>

--- parallax.js
+++ parallax.js
@@ -5,13 +5,6 @@ let kiri = document.getElementById('kiri');
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    window.addEventListener('scroll', function() {
-        const maxScroll = 4100; // Set your maximum scroll value
-        if (window.scrollY > maxScroll) {
-            window.scrollTo(0, maxScroll);
-        }
-    });
-
     text.style.marginTop = value * 1.5 + 'px';
     kanan.style.left = value * 0.5 + 'px';
     kiri.style.left = value * -0.5 + 'px';
