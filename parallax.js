53c9da749ed33f11345a6c72b3ae4b41f7f2d639
osissmabpibs <148605583+osissmabpibs@users.noreply.github.com>
2025-01-31 11:40:35 +0700

    Update parallax.js
    Signed-off-by: osissmabpibs <148605583+osissmabpibs@users.noreply.github.com>

--- parallax.js
+++ parallax.js
@@ -9,3 +9,12 @@ window.addEventListener('scroll', () => {
     kanan.style.left = value * 0.5 + 'px';
     kiri.style.left = value * -0.5 + 'px';
 })
+
+const stopScrollHeight = 6600;
+
+    // Disable scrolling when the height threshold is reached
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= stopScrollHeight) {
+        window.scrollTo(0, stopScrollHeight); // Fix the scroll position
+      }
+    });
